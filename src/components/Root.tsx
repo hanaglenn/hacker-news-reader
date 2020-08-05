5741af0cc61aadde5e8de3266e4c4d22c9f3bf17
Hana Thier <hanaglenn@gmail.com>
2020-08-05 15:27:05 -0700

    Adding some very basic styling

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import StoryList from './StoryList/index';
 import Header from './Header/index';
 
+import './base.css';
 import './root.css';
 
 export default function Root() {

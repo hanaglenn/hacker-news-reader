2fe29f982c81924c3daa3394d52db32416c96d40
Hana Thier <hanaglenn@gmail.com>
2020-08-06 11:24:30 -0700

    Using async/await in StoryService to make it more readable

--- src/services/hacker-news/index.ts
+++ src/services/hacker-news/index.ts
@@ -18,51 +18,47 @@ export class StoryService {
     this.storyIds = [];
   }
 
-  init = (): Promise<void> => {
-    return new Promise((resolve, reject) => {
-      fetch('https://hacker-news.firebaseio.com/v0/newstories.json').then((response) => {
-        response.json().then((ids) => {
-          this.storyIds = ids;
-          this.iterator = storyIterator(this.storyIds);
-          resolve();
-        }).catch((error) => {
-          throw new Error(`Error parsing response: ${error}`);
-        });
-      }).catch((error) => {
-        reject(`Error fetching new stories: ${error}`);
-      });
-    });
-  };
+  async init(): Promise<void> {
+    let ids;
+    try {
+      const response = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
+      ids = await response.json();
+    } catch (error) {
+      throw new Error(`Error fetching new stories: ${error}`);
+    }
 
-  getNextStories = (count: number): Promise<StoryId[]> => {
+    this.storyIds = ids;
+    this.iterator = storyIterator(this.storyIds);
+  }
+
+  getNextStories(count: number): Promise<StoryId[]> {
     if (!this.iterator) {
       throw new Error('Must init the StoryService before using it.');
     }
     return getNextChunk(this.iterator, count);
   }
 
-  getStory = (storyId: StoryId): Promise<Story> => {
-    return new Promise((resolve, reject) => {
-      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`).then((response) => {
-        response.json().then((story) => {
-          if (!story) {
-            throw new Error('Story does not exist.');
-          }
-          const { title, by, time, url } = story;
-          const ourStory: Story = {
-            id: storyId,
-            title,
-            author: by,
-            postedAt: time,
-            url
-          };
-          resolve(ourStory);
-        }).catch((error) => {
-          reject(`Error parsing response: ${error}`);
-        });
-      }).catch((error) => {
-        reject(`Error fetching story ${storyId}: ${error}`);
-      });
-    });
+  async getStory(storyId: StoryId): Promise<Story> {
+    let story;
+    try {
+      const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`);
+      story = await response.json();
+    } catch(error) {
+      throw new Error(`Error fetching story ${storyId}: ${error}`);
+    }
+
+    if (!story) {
+      throw new Error('Story does not exist.');
+    }
+
+    const { title, by, time, url } = story;
+    const ourStory: Story = {
+      id: storyId,
+      title,
+      author: by,
+      postedAt: time,
+      url
+    };
+    return ourStory;
   }
 }
